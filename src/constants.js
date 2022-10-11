8450f3a273d7419be1928abd14c64a77aae4ab4a
Frank-masciopinto <francesco.masciopinto@outlook.com>
2022-10-12 00:26:53 +0700

    Fixed jobs extraction

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const domain = 'https://api.saascafe.io/';
 //Production environment
 // export const domain = '';
 
-export const null_field = "N/A"
+export const null_field = 'N/A';
 
 export const LS = {
   getAllItems: () => chrome.storage.local.get(),
@@ -34,13 +34,13 @@ export function scroll_to_bottom_page() {
   window.scrollBy({
     top: 10000,
     left: 100,
-    behavior: 'smooth'
+    behavior: 'smooth',
   });
 }
 
-let CE_id = async () => await LS.getItem('CE_id')
-let minutes_interval = 30
-export const interval_check_new_job = minutes_interval*60*1000
+let CE_id = async () => await LS.getItem('CE_id');
+let minutes_interval = 30;
+export const interval_check_new_job = minutes_interval * 60 * 1000;
 export const API_ENDPOINTS = {
   check_for_new_job: domain + 'api/scraping-job?uuid=423423',
   update_job: domain + 'api/scraping-job-update',
@@ -66,60 +66,76 @@ export var notifications = {
 };
 
 function isInViewport(element) {
-  if (element ==null)
-    return false
+  if (element == null) return false;
 
   const rect = element.getBoundingClientRect();
   return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
-export function scroll_to_last_job(selector, selectorOffset=".") {
-  let jobScroll = setInterval(function(){
-    var containerElements = document.querySelectorAll(selectorOffset+selector);
-    var paginationElement =  document.querySelector(".jobs-search-results-list__pagination");
-    var seeMoreElement =  document.querySelector(".occludable-update");
-    var lastElement = containerElements[containerElements.length-1];
-    //lastElement.scrollIntoView();
-    for (var i = 0; i < containerElements.length; i++) {
-      var title_ele = containerElements[i].querySelector('.job-card-list__title');
-      //console.log("TE", title_ele);
-      if (title_ele !=null){
-        //console.log("TE INNER", title_ele.innerText);
-        if (title_ele.innerText == ""){
-          console.log("HERE in missing title...");
-          containerElements[i].scrollIntoView();
-        }else{
-          let currentEleBoundary =containerElements[i].getBoundingClientRect();
-          const y = currentEleBoundary.top + currentEleBoundary.height*3;
-          console.log("ELSE", currentEleBoundary, y, window.pageYOffset, document.body.scrollTop);
-          window.scrollTo({
-            top: y,
-            left: currentEleBoundary.left,
-            right: currentEleBoundary.right,
-            behavior: 'smooth'
-          });
-          // scroll
-          //containerElements[i].nextSibling.scrollIntoView();
+export async function scroll_to_last_job(selector, selectorOffset = '.') {
+  return new Promise(function (resolve, reject) {
+    let length = document.querySelectorAll(selectorOffset + selector).length;
+    let jobScroll = setInterval(function () {
+      var containerElements = document.querySelectorAll(
+        selectorOffset + selector
+      );
+      var paginationElement = document.querySelector(
+        '.jobs-search-results-list__pagination'
+      );
+      var seeMoreElement = document.querySelector('.occludable-update');
+      var lastElement = containerElements[containerElements.length - 1];
+      //lastElement.scrollIntoView();
+      for (var i = 0; i < containerElements.length; i++) {
+        var title_ele = containerElements[i].querySelector(
+          '.job-card-list__title'
+        );
+        //console.log("TE", title_ele);
+        if (title_ele != null) {
+          //console.log("TE INNER", title_ele.innerText);
+          if (title_ele.innerText == '') {
+            console.log('HERE in missing title...');
+            containerElements[i].scrollIntoView();
+          } else {
+            let currentEleBoundary =
+              containerElements[i].getBoundingClientRect();
+            const y = currentEleBoundary.top + currentEleBoundary.height * 3;
+            console.log(
+              'ELSE',
+              currentEleBoundary,
+              y,
+              window.pageYOffset,
+              document.body.scrollTop
+            );
+            window.scrollTo({
+              top: y,
+              left: currentEleBoundary.left,
+              right: currentEleBoundary.right,
+              behavior: 'smooth',
+            });
+            // scroll
+            //containerElements[i].nextSibling.scrollIntoView();
+          }
         }
       }
-    }
 
-    const page_result = isInViewport(paginationElement);
-    const see_more_result = isInViewport(seeMoreElement);
+      const page_result = isInViewport(paginationElement);
+      const see_more_result = isInViewport(seeMoreElement);
 
-    if (paginationElement){
-      console.log("paginationElement", page_result);
-      // clear the job scroll interval
-      clearInterval(jobScroll);
-    }
-    console.log("seeMORERESULT", see_more_result);
-    /*if(see_more_result){
+      if (paginationElement) {
+        console.log('paginationElement', page_result);
+        // clear the job scroll interval
+        clearInterval(jobScroll);
+      }
+      console.log('seeMORERESULT', see_more_result);
+      /*if(see_more_result){
       // clear the job scroll interval
       //clearInterval(jobScroll);      
     }*/
-  },500);
+    }, 500);
+  });
 }
--- src/content_Linkedin.js
+++ src/content_Linkedin.js
@@ -1,10 +1,53 @@
 'use strict';
 
 import { API } from './api.js';
-import { LS, null_field, click, scroll_to_bottom_page, scroll_to_last_job } from './constants.js';
+import { LS, null_field, click, scroll_to_bottom_page } from './constants.js';
 
 console.log('Mr_Scraper - Linkedin content script injected');
 let job_id;
+
+var oldHref = document.location.href;
+
+window.onload = function () {
+  var bodyList = document.querySelector('body');
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (oldHref != document.location.href && !oldHref.includes("?CEaewtoron=12345")) {
+        oldHref = document.location.href;
+        console.log("Previous URL doesnt have parameters");
+        if (document.URL.includes('/jobs/search/')) {
+          let waitPageLoad = setInterval(() => {
+            if (document.querySelector(".jobs-search-results-list")) {
+              chrome.runtime.sendMessage(
+                { message: 'What are the extraction rules?' },
+                (res) => {
+                  if (res) {//If background waiting for extraction
+                    console.log('ASked for Job Id to background... Repsonse: ');
+                    console.log(res.jobId);
+                    job_id = res.jobId;
+                    extract_all_jobs();
+                  }
+                }
+              );
+            clearInterval(waitPageLoad);
+            }
+          }, 2000);
+        }
+      } else if (oldHref != document.location.href) {
+        console.log("Mutation has different url from previous one...");
+        oldHref = document.location.href;
+      }
+    });
+  });
+
+  var config = {
+    childList: true,
+    subtree: true,
+  };
+
+  observer.observe(bodyList, config);
+};
+
 async function startAutomation(rules) {
   console.log('Start Automation with rules: ');
   console.log(rules);
@@ -62,7 +105,7 @@ async function startAutomation(rules) {
   ];
   let extracted_info = {};
   let status;
-  let uuid = await LS.getItem("uuid");
+  let uuid = await LS.getItem('uuid');
   try {
     for (let i = 0; i < rules.length; i++) {
       console.log('Rule: \n' + rules[i]);
@@ -93,20 +136,19 @@ async function startAutomation(rules) {
         }
       }
     }
-    status = "success";
-  }
-  catch (e) {
+    status = 'success';
+  } catch (e) {
     console.log('ERROR: ' + e.message);
-    status = "error";
+    status = 'failure';
   }
-  console.log('Extracted info: \n', extracted_info); 
+  console.log('Extracted info: \n', extracted_info);
   let payload = {
-    "uuid": await LS.getItem('CE_uuid'),
-    "job_id": job_id,
-    "response": extracted_info,
-    "status": status
+    uuid: await LS.getItem('CE_uuid'),
+    job_id: job_id,
+    response: extracted_info,
+    status: status,
   };
-  API.update_job(payload);
+  API.update_job(payload).then(window.close());
 }
 
 function extract_querySelector(rule) {
@@ -229,93 +271,111 @@ async function extract_social_posts() {
   console.log('Number of posts Extracted: ', extracted_posts.length);
   console.log(extracted_posts);
   let payload = {
-    "uuid": await LS.getItem('CE_uuid'),
-    "job_id": job_id,
-    "response": extracted_posts,
-    "status": "success"
+    uuid: await LS.getItem('CE_uuid'),
+    job_id: job_id,
+    response: extracted_posts,
+    status: 'success',
   };
-  API.update_job(payload);
+  API.update_job(payload).then(window.close());
 }
 
-async function fetch_page_jobs(extracted_jobs){
-  // scroll to bottom of jobs container to get all the jobs in viewport
-  scroll_to_last_job('artdeco-entity-lockup__title', '.');
+async function scroll_to_last_job() {
   return new Promise(function (resolve, reject) {
-    console.log("Let jobs scroll to last");
+    //Scroll to last job
+    document.querySelector('.jobs-search-results-list').scrollBy({
+      top: 10000,
+      left: 100,
+      behavior: 'smooth',
+    });
+    setTimeout(() => {
+      document.querySelector('.jobs-search-results-list').scrollBy({
+        top: 10000,
+        left: 100,
+        behavior: 'smooth',
+      });
+    }, 2000);
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+}
 
-    setTimeout(function(){
-      let all_jobs = document.querySelectorAll('.jobs-search-results__list-item');
-      // loop over all the jobs available in a container
-      for (let i = 0; i < all_jobs.length; i++) {
-        var job_title = "";
-        var job_location = "";
-        var title_ele = all_jobs[i].querySelector('.job-card-list__title');
-        var location_ele  = all_jobs[i].querySelector(".artdeco-entity-lockup__caption");
-        if (title_ele !=null)
-          job_title = title_ele.innerText.trim();
+async function fetch_page_jobs(extracted_jobs) {
+  // scroll to bottom of jobs container to get all the jobs in viewport
+  return new Promise(function (resolve, reject) {
+    console.log('Let jobs scroll to last');
+    let all_jobs = document.querySelectorAll('.jobs-search-results__list-item');
+    // loop over all the jobs available in a container
+    for (let i = 0; i < all_jobs.length; i++) {
+      var job_title = '';
+      var job_location = '';
+      var title_ele = all_jobs[i].querySelector('.job-card-list__title');
+      var location_ele = all_jobs[i].querySelector(
+        '.artdeco-entity-lockup__caption'
+      );
+      if (title_ele != null) job_title = title_ele.innerText.trim();
 
-        if (location_ele !=null){
-          job_location = location_ele.innerText.trim();
-          if (job_location.indexOf("\n") != -1)
-            job_location = job_location.replace("\n", " "); 
-        }
-
-        if (job_title.length){
-          extracted_jobs.push({
-            "job_title":job_title,
-            "job_location":job_location,
-          });
-          resolve();     
-        }
+      if (location_ele != null) {
+        job_location = location_ele.innerText.trim();
+        if (job_location.indexOf('\n') != -1)
+          job_location = job_location.replace('\n', ' ');
       }
-    },10000);
+
+      if (job_title.length) {
+        extracted_jobs.push({
+          job_title: job_title,
+          job_location: job_location,
+        });
+        resolve();
+      }
+    }
   });
 }
 
 async function extract_all_jobs() {
   let extracted_jobs = [];
   // scroll to bottom of jobs container to see pagination element
-  //scroll_to_last_job('jobs-search-results__list-item', '.');
+  await scroll_to_last_job();
   // check if pagination element exists or not
-  let pagination_ele = document.querySelector(".jobs-search-results-list__pagination");
-  console.log("IN PAG", pagination_ele);
-  if (pagination_ele != null) {
-    console.log("IN PAG NULL ");
-    // means we have pagination ele
-    let last_li_ele = pagination_ele.querySelector("li.artdeco-pagination__indicator:last-child");
-    if (last_li_ele != null){
-      var last_page = last_li_ele.getAttribute("data-test-pagination-page-btn");
-      if (last_page){
-        for (var j = 1; j < last_page; j++) {
-          var job_start = (j - 1)*25;
-          var current_url = document.URL;
-          var next_job_url = "";
-          if (current_url.indexOf("start") !== -1)
-            next_job_url = current_url.split("start")[0].concat("start="+job_start);
-          else
-            next_job_url = current_url.concat("start="+job_start);
-        }
+  let pagination_ele = document.querySelector(
+    '.jobs-search-results-list__pagination'
+  );
+  let status;
+  try {
+    if (pagination_ele != null) {
+      console.log('Multiple Jobs pages exist');
+      // means we have pagination element
+      let last_li_ele = pagination_ele.querySelector(
+        'li.artdeco-pagination__indicator:last-child'
+      );
+      if (last_li_ele != null) {
+        var last_page_number = parseInt(
+          last_li_ele.getAttribute('data-test-pagination-page-btn')
+        );
+        await fetch_page_jobs(extracted_jobs);
+        console.log(extracted_jobs);
       }
-    }    
+    } else {
+      console.log('We have only jobs on the current page');
+      await fetch_page_jobs(extracted_jobs);
+      console.log(extracted_jobs);
+    }
+    status = 'success';
+  } catch {
+    status = 'failure';
   }
-  else{
-    console.log("I M HERE");
-    // we have only jobs on the current page
-    await fetch_page_jobs(extracted_jobs);
-    console.log("JON DSTA", extracted_jobs, extracted_jobs.length);
-  }
+  let payload = {
+    uuid: await LS.getItem('CE_uuid'),
+    job_id: job_id,
+    response: extracted_jobs,
+    status: status,
+  };
+  API.update_job(payload).then(window.close());
+
 }
-
-
-extract_all_jobs()
-
 //startAutomation();
 if (document.URL.includes('?CEaewtoron=12345')) {
   if (document.URL.includes('/posts/')) {
-    // scrape jobs here
-    let job_exists = extract_regex(rules[i].rule);
-    console.log('Extracted value: ' + extracted_value);
-
     setTimeout(() => {
       scroll_to_bottom_page();
     }, 3000);
@@ -324,10 +384,32 @@ if (document.URL.includes('?CEaewtoron=12345')) {
         { message: 'What are the extraction rules?' },
         (res) => {
           job_id = res.jobId;
-          extract_social_posts()
+          extract_social_posts();
         }
       );
     }, 5000);
+  } else if (document.URL.includes('/jobs/') && !document.URL.includes('currentJobId')) {
+    //Open All Jobs page for extraction
+    try {
+      document.querySelector('a[href*="/jobs/search/?"]').click()
+    }
+    catch {
+      chrome.runtime.sendMessage(
+        { message: 'What are the extraction rules?' },
+        async (res) => {
+          console.log('Background response: ');
+          console.log(res);
+          job_id = res.jobId;
+          let payload = {
+            uuid: await LS.getItem('CE_uuid'),
+            job_id: job_id,
+            response: [],
+            status: "Failure - No Jobs Found",
+          };
+          API.update_job(payload).then(window.close());
+        }
+      );
+    }
   } else {
     chrome.runtime.sendMessage(
       { message: 'What are the extraction rules?' },
